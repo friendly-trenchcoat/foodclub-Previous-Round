5e7fd6d0e1d59103314d7fbf17455ac74e289f05
friendly-trenchcoat <tippywigwom@yahoo.com>
2016-07-30 18:51:27 -0500

    Create fcPrevRound.user.js

---
+++ fcPrevRound.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name           Neopets - Food Club Previous Round
+// @author         friendly-trenchcoat
+// @description    Lists previous round's winners on one page. May take a second.
+// @include        http://www.neopets.com/pirates/foodclub.phtml?type=previous
+// @require	       http://code.jquery.com/jquery-latest.min.js
+// ==/UserScript==
+
+$("img[src*='shipwreck']").hide();
+$("table[width='500']").width(750);
+$("font[color='white']").text("All Competitions").wrap('<b></b>');
+$("font[color='darkred']").hide();
+
+var winnerName, winnerPic, winnerArena;
+var firstName = $("font[color='darkred']").next().next();
+var firstPic = $("img[src*='/fc_pirate']");
+var arenas = ["Lagoon","Treasure Island","Hidden Cove","Harpoon Harry's"];
+var names = new Array(4);
+var pics = new Array(4);
+
+for (var i=2; i<6; i++){
+    $.ajax({
+        url: 'http://www.neopets.com/pirates/foodclub.phtml?type=previous&id='+i,
+        dataType: 'text',
+        success: function(data) {
+            winnerName = $(data).find("font[color='darkred']").next().next().text();
+            winnerPic = $(data).find("img[src*='/fc_pirate']").clone();
+            winnerArena = arenas.indexOf($(data).find("font[color='white']").text().replace(" Competition",""));  // for order, because ajax is unpredictable
+            names[winnerArena] = winnerName;
+            pics[winnerArena] = winnerPic;
+        }
+    });
+}
+$(document).ajaxStop(function(){  // after all ajax is done
+    for (var j=3; j>=0; j--){
+        firstName.after(' | '+names[j].bold());
+        firstPic.after(pics[j]);
+    }
+});
